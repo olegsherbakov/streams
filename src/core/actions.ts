d6e06fade2d2d1afe58c9d6fa80101ccad357272
olegsherbakov <sherbakov.olegik@gmail.com>
2021-01-10 15:51:31 +0300

    install rxjs

--- src/core/actions.ts
+++ src/core/actions.ts
@@ -1,3 +1,5 @@
+import { pipe, of } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { ActionCreator } from 'redux'
 import { ThunkAction } from 'redux-thunk'
 
@@ -10,6 +12,11 @@ export const switchOff: ActionCreator<ThunkResult<ActionTypes>> = () => {
     const {
       system: { isOn },
     } = getState()
+
+    // TODO dev
+    console.log(`?pipe`, pipe)
+    console.log(`?of`, of)
+    console.log(`?map`, map)
 
     return dispatch({
       type: isOn ? ACTIONS.OFF : ACTIONS.ON,
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,32 @@ const yargs = require('yargs')
 const MiniCssExtractPlugin = require(`mini-css-extract-plugin`)
 const isProduction = yargs.argv.mode === `production`
 
+function rxjsExternals(context, request, callback) {
+  if (request.match(/^rxjs(\/|$)/)) {
+    var parts = request.split('/')
+    if (parts.length > 2) {
+      console.warn(
+        'webpack-rxjs-externals no longer supports v5-style deep imports like rxjs/operator/map etc. It only supports rxjs v6 pipeable imports via rxjs/operators or from the root.'
+      )
+    }
+
+    return callback(null, {
+      root: parts,
+      commonjs: request,
+      commonjs2: request,
+      amd: request,
+    })
+  }
+
+  callback()
+}
+
 module.exports = {
   mode: isProduction ? `production` : `development`,
   devtool: `source-map`,
   resolve: {
     alias: {
-      'src': path.resolve(__dirname, `src/`),
+      src: path.resolve(__dirname, `src/`),
       '@core': path.resolve(__dirname, `src/core/`),
       '@views': path.resolve(__dirname, `src/views/`),
       '@styles': path.resolve(__dirname, `src/styles/`),
@@ -71,12 +91,17 @@ module.exports = {
       filename: `styles.css`,
     }),
   ],
-  externals: {
-    'react': `React`,
-    'react-dom': `ReactDOM`,
-    'redux': `Redux`,
-    'react-redux': `ReactRedux`,
-  },
+  externals: [
+    rxjsExternals,
+    {
+      react: `React`,
+      'react-dom': `ReactDOM`,
+      redux: `Redux`,
+      'react-redux': `ReactRedux`,
+      rxjs: `rxjs`,
+      'rxjs/operators': `rxjs.operators`,
+    },
+  ],
   output: {
     library: `Streams`,
     libraryTarget: `umd`,
